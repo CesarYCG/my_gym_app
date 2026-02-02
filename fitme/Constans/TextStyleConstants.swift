4aa9a0254157c3e7d1ecf057a0a9af0e952356f4
Cesar Yair <cesar.yair.cg@gmail.com>
2026-02-02 17:21:21 -0600

    Add file for centralized text styles

---
+++ fitme/Constans/TextStyleConstants.swift
@@ -0,0 +1,12 @@
+//
+//  Text.swift
+//  fitme
+//
+//  Created by César Yair Calderón Guevara on 02/02/26.
+//  This file contains Text centralized patterns and
+//  designs for consistency among all Text objects in the app.
+//
+
+
+
+
