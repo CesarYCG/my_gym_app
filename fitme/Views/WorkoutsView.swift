ba43f134d32447cc44b35ef2d9c251c5165cef87
Cesar Yair <cesar.yair.cg@gmail.com>
2026-02-02 21:32:53 -0600

    Update WorkoutsView view

---
+++ fitme/Views/WorkoutsView.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutsView.swift
+//  fitme
+//
+//  Created by César Yair Calderón Guevara on 02/02/26.
+//
+import SwiftUI
+
+struct WorkoutsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Your Saved Workouts")
+                .navigationTitle("Workouts")
+        }
+    }
+}
