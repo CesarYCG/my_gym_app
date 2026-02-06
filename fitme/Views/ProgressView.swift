4a34b50333effe378ce9f0fbd1321efe60f58cc1
Cesar Yair <cesar.yair.cg@gmail.com>
2026-02-05 22:02:00 -0600

    Added Progress view template

---
+++ fitme/Views/ProgressView.swift
@@ -0,0 +1,13 @@
+//
+//  ProgressView.swift
+//  fitme
+//
+//  Created by César Yair Calderón Guevara on 05/02/26.
+//
+import SwiftUI
+
+struct ProgressView : View {
+    var body: some View {
+        Text("Your Progress data will appear here!")
+    }
+}
