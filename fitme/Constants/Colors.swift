085e27a39b4ac72a612ddc5add628fb9579f51f8
Cesar Yair <cesar.yair.cg@gmail.com>
2026-02-06 22:04:59 -0600

    Rename TextStyleConstants -> Colors.swift

---
+++ fitme/Constants/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  fitme
+//
+//  Created by César Yair Calderón Guevara on 02/02/26.
+//  This file contains centralized color constants and
+//  designs for consistency among all Color usage in the app.
+//
+
+import SwiftUI
+
+// Colours: - App Color Palette
+extension Color {
+    // Brand Accent Color
+    static let emerald: Color = Color(red: 0.063, green: 0.725, blue: 0.506)
+    static let purpleBlue: Color = Color(red: 0.318, green: 0.267, blue: 0.929)
+    static let tealCyan: Color = Color(red: 0.247, green: 0.859, blue: 0.702)
+    static let yellowLime: Color = Color(red: 0.886, green: 0.843, blue: 0.341)
+    static let magentaPink: Color = Color(red: 0.863, green: 0.286, blue: 0.847)
+    static let orangeCoral: Color = Color(red: 0.937, green: 0.267, blue: 0.929)
+}
+
