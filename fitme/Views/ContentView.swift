142b13566a40d8da6b2c63acb46e5c25fe89a083
Cesar Yair <cesar.yair.cg@gmail.com>
2026-04-06 20:27:49 -0600

    Modified Logic to validate if user is Authenticated

--- fitme/Views/ContentView.swift
+++ fitme/Views/ContentView.swift
@@ -1,53 +1,39 @@
-//
-//  ContentView.swift
-//  fitme
-//
-//  Created by César Yair Calderón Guevara on 30/01/26.
-//
-
 import SwiftUI
 import Supabase
 
 struct ContentView: View {
+    // Listeners for Supabase Auth
+    @State private var isAuthenticated = false 
+    @State private var isCheckingSession = true
+    
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            
-            WorkoutsView()
-                .tabItem {
-                    Label("Workouts", systemImage: "dumbbell")
-                }
-            
-            UserProgressView()
-                .tabItem {
-                    Label("Progress", systemImage: "chart.line.uptrend.xyaxis")
-                }
-        } // Just A quick try for settup of DB
-        .onAppear {
-            Task {
-                do {
-                    // Test fetch to the "User" table
-                    let response = try await supabase
-                        .from("User")
-                        .select()
-                        .execute()
-                    
-                    if let stringData = String(data: response.data, encoding: .utf8) {
-                        print("✅ Supabase Connection Successful!")
-                        print("Data in 'User': \(stringData)")
-                    }
-                } catch {
-                    print("❌ Supabase Connection Failed:")
-                    print(error.localizedDescription)
-                }
+        Group {
+            if isCheckingSession {
+                // Loading screen while checking the token
+                ProgressView("Connecting...")
+            } else if isAuthenticated {
+                // If user is logged in, show the main tab view
+                MainTabView() 
+            } else {
+                // Else, go to sign in
+                LoginView(isAuthenticated: $isAuthenticated)
             }
         }
+        .onAppear {
+            checkCurrentSession()
+        }
     }
-}
-
-#Preview {
-    ContentView()
+    
+    func checkCurrentSession() {
+        Task {
+            // Check if there is an active session in Supabase
+            do {
+                _ = try await supabase.auth.session
+                isAuthenticated = true
+            } catch {
+                isAuthenticated = false
+            }
+            isCheckingSession = false
+        }
+    }
 }
